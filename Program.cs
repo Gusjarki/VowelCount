caea5b7db8df216b4699a4d2ad83c733cc60d28b
Gusjarki <desm.g@yandex.ru>
2022-07-04 21:54:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*Вернуть количество  гласных в заданной строке.
+ * Мы будем рассматривать a, e, i, o, u как гласные  (но не y).
+ * Входная строка будет состоять только из строчных букв и/или пробелов.
+ */
+
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        foreach (char vowel in str)
+        {
+            if (vowels.Contains(vowel))
+            {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
+}
